fce3d06b67513a4ebee8bf24749fb74270b8e04b
gcshivathanu <sivathanu.gopirajan.ext@siemens.com>
2019-10-13 16:11:38 +0200

    dev : Modified the response of paginate results

--- controllers/paginate.js
+++ controllers/paginate.js
@@ -13,14 +13,18 @@ router.get('/:pageNo', function (request, response) {
     var perPage = 10, page = Math.max(0, request.param('pageNo'));
     var query = Paginate.find({}).skip(page * perPage).limit(perPage);
     Paginate.count({}, function (error, totalCount) {
+        var result = {};
         query.exec(function (error, paginate) {
             if (error) {
                 response.send(error);
             }
-            paginate.unshift({ totalCount: totalCount, pageNo: page });
-            response.send(paginate);
+            result.pageNo = page + 1;
+            result.totalCount = totalCount;
+            result.perPageCount = paginate.length;
+            result.data = paginate;
+            response.send(result);
         });
-    });    
+    });
 });
 
 module.exports = router;
